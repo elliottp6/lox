dbea5e4b6542f326103daa968c940a4e75c513b4
Elliott Prechter <elliott.prechter@gmail.com>
2022-05-20 15:33:20 -0400

    16.2: a token at a time

--- clox/compiler.c
+++ clox/compiler.c
@@ -5,4 +5,15 @@
 
 void compile( const char* source ) {
     initScanner( source );
+    for( int line = -1;; ) {
+        Token token = scanToken();
+        if( token.line != line ) {
+            printf( "%4d ", token.line );
+            line = token.line;
+        } else {
+            printf("   | ");
+        }
+        printf( "%2d '%.*s'\n", token.type, token.length, token.start ); 
+        if( TOKEN_EOF == token.type ) break;
+    }
 }
--- clox/scanner.c
+++ clox/scanner.c
@@ -4,9 +4,9 @@
 #include "scanner.h"
 
 typedef struct {
-  const char* start;
-  const char* current;
-  int line;
+    const char* start;
+    const char* current;
+    int line;
 } Scanner;
 
 Scanner scanner;
@@ -15,4 +15,36 @@ void initScanner( const char* source ) {
     scanner.start = source;
     scanner.current = source;
     scanner.line = 1;
+}
+
+static bool isAtEnd() { return '\0' == *scanner.current; }
+
+static Token makeToken( TokenType type ) {
+    Token token;
+    token.type = type;
+    token.start = scanner.start;
+    token.length = (int)(scanner.current - scanner.start);
+    token.line = scanner.line;
+    return token;
+}
+
+static Token errorToken( const char* message ) {
+    Token token;
+    token.type = TOKEN_ERROR;
+    token.start = message;
+    token.length = (int)strlen( message );
+    token.line = scanner.line;
+    return token;
+}
+
+Token scanToken() {
+    // check for termination
+    scanner.start = scanner.current;
+    if( isAtEnd() ) return makeToken( TOKEN_EOF );
+    
+    // advance
+    // TODO
+    
+    // could not tokenize
+    return errorToken( "Unexpected character" );
 }
